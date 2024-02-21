35ea40c8fdfe1567aae91074b491e745339d8714
NoumanZaman <noumanzamansketches@gmail.com>
2024-02-21 14:06:55 +0000

    add new file

---
+++ new.py
@@ -0,0 +1,34 @@
+# number : int = int(input("Please Enter Your Number: "))
+# if number == 2:
+#      print(f"You Enter {number}")
+#      print("Number Two")
+# else:
+#     print(f"Number is not 2 it is {number}")
+# print("It is a normal statement")  
+
+
+
+# integer : int = int(input("Please Enter Your Number:"))
+# if integer == 2:
+#      print(f"you Enter {integer}")
+     
+# else: 
+#      print(f"Number is not 2 it is {integer}")
+
+
+# students : list = ["sohaib" , "nouman" , "umer" , "hamza"]
+# age : list = [20,30,19,60]
+# course : list = ["python" , "c" , "cplus", "java"] 
+# print(students)
+# print(age)
+# print(course)
+# print(f''' my name is {students[0]}
+#  my age is {age[1]}
+#  and my course is {course[0]}''')
+
+list : list = [ 1,2,4,5,44,6,67]
+list[2:6] = [10, 0]
+print(list)
+
+
+# [1,2,4,5,10,6,0]
